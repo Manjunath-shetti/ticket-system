101d84760b05c4f9e8fb5eec96d5cf523243786e
Manjunath <pmanjunathshetti@gmail.com>
2022-10-26 00:01:50 +0100

    adding department flask apis

--- backend-project/main.py
+++ backend-project/main.py
@@ -3,9 +3,11 @@ __auther__ = 'Manjunath Shetty'
 from flask import Flask,request
 import dbconfiguration
 from service import UserOperationService
+from service import DepartmentOperationService
 
 app = Flask(__name__)
 
+#************************************USER ENDPOINTS*******************************************
 #login api 
 @app.route('/login',methods=['POST'])
 def loginController():
@@ -35,6 +37,32 @@ def updateUserController():
 def deleteUserController():
     email = request.form['email']
     return UserOperationService.deleteUserFunction(email)
+
+
+#************************************DEPARTMENT ENDPOINTS*******************************************
+#add new department
+@app.route('/adddepartment',methods=['POST'])
+def addDepartmentController():
+    deptname = request.form['name']
+    return DepartmentOperationService.addDepartmentFunction(deptname)
+
+#update new department
+@app.route('/updatedepartment',methods=['PUT'])
+def updateDepartmentController():
+    deptname = request.form['name']
+    id = request.form['id']
+    return DepartmentOperationService.updateDepartmentFunction(id,deptname) 
+
+#delete department     
+@app.route('/deletedepartment',methods=['DELETE'])
+def deleteDepartmentController():
+    id = request.form['id']
+    return DepartmentOperationService.deleteDepartmentFunction(id)
+
+#get all department 
+@app.route('/getalldepartment',methods=['GET'])
+def getAllDepartmentController():
+    return DepartmentOperationService.getAllDepartmentFunction()
 
 if __name__ == "__main__":
     app.run()
---
+++ backend-project/service/DepartmentOperationService.py
@@ -0,0 +1,109 @@
+from ast import main
+
+from flask import jsonify
+import dbconfiguration
+import json
+
+def addDepartmentFunction(name):
+    department_Dict = {}
+    department_Dict['id']=""
+    department_Dict['departmentName'] = ""
+    department_Dict['status'] = False
+    department_Dict['message']="Failed to add department"
+
+    instance = dbconfiguration.getDBInstance()
+
+    cursor = instance.cursor()
+    
+    cursor.execute("insert into department(name) values(%s)",(name,))
+    instance.commit()
+    cursor.execute('SELECT last_insert_id()')
+    id = cursor.fetchone()
+    dbconfiguration.closeDBInstance(cursor)
+    department_Dict['id'] = id[0]
+    department_Dict['First_Name'] = name
+    department_Dict['status'] = True
+    department_Dict['message']="Success"
+
+    return department_Dict
+
+def updateDepartmentFunction(id,name):
+    department_Dict = {}
+    department_Dict['id']=""
+    department_Dict['departmentName'] = ""
+    department_Dict['status'] = False
+    department_Dict['message']="Failed to update department"
+
+    instance = dbconfiguration.getDBInstance()
+    cursor = instance.cursor()
+
+    cursor.execute("select * from department where id = %s", (id,))
+    
+    departmentInfo = cursor.fetchall()
+    if len(departmentInfo) == 0:        
+        department_Dict['message'] = "Department does not exist!"
+        return department_Dict
+    
+    cursor.execute("update department set name = %s where id = %s",(name,id,))
+    instance.commit()
+    dbconfiguration.closeDBInstance(cursor)
+    department_Dict['id'] = id
+    department_Dict['departmentName'] = name
+    department_Dict['status'] = True
+    department_Dict['message']="Success"
+
+    return department_Dict
+
+def deleteDepartmentFunction(id):
+    department_Dict = {}
+    department_Dict['id']=""
+    department_Dict['status'] = False
+    department_Dict['message']="Failed to delete department"
+
+    instance = dbconfiguration.getDBInstance()
+    cursor = instance.cursor()
+
+    cursor.execute("select * from department where id = %s", (id,))
+    
+    departmentInfo = cursor.fetchall()
+    if len(departmentInfo) == 0:        
+        department_Dict['message'] = "Department does not exist!"
+        return department_Dict
+    
+    cursor.execute("delete from department where id = %s",(id,))
+    instance.commit()
+    dbconfiguration.closeDBInstance(cursor)
+    department_Dict['id'] = id
+    department_Dict['status'] = True
+    department_Dict['message']="Success"
+
+    return department_Dict
+
+def getAllDepartmentFunction():
+    main_dict = {}
+    departmentList = []
+    main_dict['status'] = False
+    main_dict['message']="Failed to delete department"
+
+    instance = dbconfiguration.getDBInstance()
+    cursor = instance.cursor()
+
+    cursor.execute("select * from department")
+    
+    departmentInfo = cursor.fetchall()
+    dbconfiguration.closeDBInstance(cursor)
+    if len(departmentInfo) == 0:        
+        department_Dict['message'] = "Department does not exist!"
+        return main_dict
+    
+    for i in range(0,len(departmentInfo)):
+        department_Dict = {}
+        department_Dict['id'] = departmentInfo[i][0]
+        department_Dict['name'] = departmentInfo[i][1]
+        departmentList.append(department_Dict)
+
+    main_dict['data'] = departmentList
+    main_dict['status'] = True
+    main_dict['message']="Success"
+
+    return main_dict
